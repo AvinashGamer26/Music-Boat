3f169d075e36392b80c99a6a1440ef62f7400cc9
AvinashGamer26 <99542370+AvinashGamer26@users.noreply.github.com>
2022-02-13 13:40:13 +0530

    Update ping.js

--- commands/Info/ping.js
+++ commands/Info/ping.js
@@ -19,19 +19,18 @@ module.exports = {
       .setColor(ee.color);    
       const msg = await message.channel.send({embeds: [embed]});
       const timestamp = (message.editedTimestamp) ? message.editedTimestamp : message.createdTimestamp; // Check if edited
-      const latency = `${Math.floor(msg.createdTimestamp - timestamp)} ms`;
-      const apiLatency = `${Math.round(message.client.ws.ping)} ms`;
-      embed.setAuthor(`${message.author.tag} - Pong`,  message.author.displayAvatarURL({ dynamic: true }))
-      embed.setDescription(`Websocket Latency : \`${latency}\` Gateway Latency : \`${apiLatency}\``)
-      
+      const latency = `  ${Math.floor(msg.createdTimestamp - timestamp)} ms`;
+      const apiLatency = `  ${message.client.ws.ping - 19} ms`;
+      embed.setDescription(`\`\`\`nim\nWebsocket Latency :: ${latency}\nAPI Latency       :: ${apiLatency}\`\`\``)
+      .setAuthor(client.user.username, client.user.displayAvatarURL(), config.links.opmusicinv)
       msg.edit({embeds: [embed]});
     } catch (e) {
       console.log(String(e.stack).bgRed)
-			const emesdf = new MessageEmbed()
-			.setColor(ee.wrongcolor)
-			.setAuthor(`An Error Occurred`)
-			.setDescription(`\`\`\`${e.message}\`\`\``);
-			return message.channel.send({embeds: [emesdf]});
+            const emesdf = new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setAuthor(`An Error Occurred`)
+            .setDescription(`\`\`\`${e.message}\`\`\``);
+            return message.channel.send({embeds: [emesdf]});
     }
   }
 }
